b123c4b876faedf7d9e7441726ee22a5b42fd607
Melvillian <melvillevt@gmail.com>
2020-03-13 16:54:34 -0400

    implemented CondVxEq

--- src/lib.rs
+++ src/lib.rs
@@ -51,22 +51,7 @@ pub struct Interpreter {
     pc: usize,          // program counter, 16 bits are needed but we use usize so we can index with it
 
     // 16 8-bit registers. VF is used as a flag by several of the opcodes (see @Op)
-    v0: u8,
-    v1: u8,
-    v2: u8,
-    v3: u8,
-    v4: u8,
-    v5: u8,
-    v6: u8,
-    v7: u8,
-    v8: u8,
-    v9: u8,
-    va: u8,
-    vb: u8,
-    vc: u8,
-    vd: u8,
-    ve: u8,
-    vf: u8,
+    v: [u8; 16],
 
     graphics: [u8; 64 * 32], // 64x32 pixel monochrome screen
 
@@ -78,6 +63,11 @@ pub struct Interpreter {
     font_set: [[u8; 5]; 2],  // stores the 16 5-byte hex font set
 }
 
+#[macro_export]
+macro_rules! reg {
+    ($i:literal) => (self.v$i);
+}
+
 impl Interpreter {
     pub fn new() -> Self {
         Interpreter {
@@ -85,22 +75,7 @@ impl Interpreter {
             sp: 0,
             instr: 0,
             pc: 0,
-            v0: 0,
-            v1: 0,
-            v2: 0,
-            v3: 0,
-            v4: 0,
-            v5: 0,
-            v6: 0,
-            v7: 0,
-            v8: 0,
-            v9: 0,
-            va: 0,
-            vb: 0,
-            vc: 0,
-            vd: 0,
-            ve: 0,
-            vf: 0,
+            v: [0; 16],
             graphics: [0; 64 * 32],
             delay_timer: 0,
             sound_timer: 0,
@@ -127,7 +102,7 @@ impl Interpreter {
                 }
 
                 if set_vf {
-                    self.vf = 1;
+                    self.v[0xf] = 1;
                 }
             },
             Op::Return => {
@@ -157,6 +132,14 @@ impl Interpreter {
 
                 // jump to the subroutine
                 self.pc = three_u8s_to_usize(msb, b, lsb);
+            },
+            Op::CondVxEq(msb, b, lsb) => {
+                let reg = self.v[msb as usize];
+                let byte = two_u8s_to_u16(b, lsb) as u8;
+
+                if (reg == byte) {
+                    self.pc = self.pc + 2;
+                }
             }
             _ => unimplemented!()
         }
@@ -186,7 +169,7 @@ impl Interpreter {
 
         self.execute(op);
 
-        if self.vf == 1 { // a display bit changed by the previous Op?
+        if self.v[0xf] == 1 { // a display bit changed by the previous Op?
             self.draw();
         }
 
@@ -281,7 +264,7 @@ fn usize_to_three_u8s(u: usize) -> (u8, u8, u8) {
 
 
 #[cfg(test)]
-mod interpreter {
+mod interpreter_tests {
     use super::*;
 
     mod execute {
@@ -289,17 +272,17 @@ mod interpreter {
         #[test]
         fn display_clear_op() {
             let mut interpreter = Interpreter::new();
-            assert_eq!(interpreter.vf, 0);
+            assert_eq!(interpreter.v[0xf], 0);
 
             interpreter.execute(Op::DispClear);
             for i in 0..interpreter.graphics.len() {
                 assert_eq!(interpreter.graphics[i], 0);
             }
-            assert_eq!(interpreter.vf, 0);
+            assert_eq!(interpreter.v[0xf], 0);
 
             interpreter.graphics[0] = 1;
             interpreter.execute(Op::DispClear);
-            assert_eq!(interpreter.vf, 1);
+            assert_eq!(interpreter.v[0xf], 1);
         }
 
         #[test]
@@ -342,6 +325,7 @@ mod interpreter {
 
         #[test]
         fn call_subroutine_op() {
+            let mut interpreter = Interpreter::new();
             assert_eq!(interpreter.pc, 0);
             assert_eq!(interpreter.sp, 0);
 
@@ -361,9 +345,39 @@ mod interpreter {
         }
 
         #[test]
-        fn cond_vx_eq_op() {
+        fn cond_vx_eq_op_false() {
             let mut interpreter = Interpreter::new();
-            g
+
+            // setup test
+
+            let nibbles = 0xFAB; // arbitrary 3 nibbles
+            let (msb, b, lsb) = usize_to_three_u8s(nibbles);
+            let msb_usize = msb as usize;
+            interpreter.pc = STARTING_MEMORY_BYTE;
+
+            assert_eq!(interpreter.v[msb_usize], 0);
+
+            interpreter.execute(Op::CondVxEq(msb, b, lsb));
+
+            assert_eq!(interpreter.pc, STARTING_MEMORY_BYTE);
+        }
+
+        #[test]
+        fn cond_vx_eq_op_true() {
+            let mut interpreter = Interpreter::new();
+
+            // setup test
+
+            let nibbles = 0xF00; // arbitrary 3 nibbles
+            let (msb, b, lsb) = usize_to_three_u8s(nibbles);
+            let msb_usize = msb as usize;
+            interpreter.pc = STARTING_MEMORY_BYTE;
+
+            assert_eq!(interpreter.v[msb_usize], 0);
+
+            interpreter.execute(Op::CondVxEq(msb, b, lsb));
+
+            assert_eq!(interpreter.pc, STARTING_MEMORY_BYTE + 2);
         }
     }
 
