f1a038faec946ddc1fb7fde5258ee4cd00593109
Melvillian <melvillevt@gmail.com>
2020-03-28 08:45:44 -0400

    refactor keyboard.rs to use Key enum where before it used minifb::Key.
    Right now the chipotle8 'cargo build' is breaking because we still need to update lib.rs to use keyboard::Key.
    That will happen in the next commit.

--- src/keyboard.rs
+++ src/keyboard.rs
@@ -1,4 +1,3 @@
-use minifb::Key as Mini_Key;
 use std::collections::HashMap;
 use std::collections::HashSet;
 use std::iter::FromIterator;
@@ -18,7 +17,7 @@ const NUM_KEYS: usize = 16;
 /// +-+-+-+-+                +-+-+-+-+
 /// |A|0|B|F|                |Z|X|C|V|
 /// +-+-+-+-+                +-+-+-+-+
-#[derive(Eq, PartialEq, Hash)]
+#[derive(Eq, PartialEq, Hash, Clone, Copy)]
 pub enum Key {
     Key1,
     Key2,
@@ -38,115 +37,93 @@ pub enum Key {
     F,
 }
 
+impl Key {
+    fn variants() -> Vec<Key> {
+        vec![Self::Key1,
+             Self::Key2,
+             Self::Key3,
+             Self::C,
+             Self::Key4,
+             Self::Key5,
+             Self::Key6,
+             Self::D,
+             Self::Key7,
+             Self::Key8,
+             Self::Key9,
+             Self::E,
+             Self::A,
+             Self::Key0,
+             Self::B,
+             Self::F,
+        ]
+    }
+}
 /// Contains the state (up or down) of the CHIP-8's 16 keys, as well as any
 /// state related to keyboard input
 pub struct Keyboard {
-    key_input: HashMap<usize, bool>, // 16 bit hex keyboard input (0-F).
-    // Each bit stores the 1 (on) or 0 (off) keypress state
+    key_input: HashMap<Key, bool>, // 16 bit hex keyboard input (0-F). true if pressed otherwise false
     fx0a_metadata: FX0AMetadata, // used to store state for instruction FX0A,
 }
 
 impl Keyboard {
     pub fn new() -> Self {
-        let mut key_input = HashMap::with_capacity(NUM_KEYS);
-        for idx in 0..NUM_KEYS {
-            key_input.insert(idx, false);
-        }
+        let mut key_input: HashMap<Key, bool> = Key::variants()
+            .iter()
+            .map(|k| (*k, false))
+            .collect();
 
         Keyboard {
             key_input,
             fx0a_metadata: FX0AMetadata {
                 should_block_on_keypress: false,
-                last_key_pressed: None,
                 register: None,
             },
         }
     }
 
-    /// We use the following mapping for the 16 bit hex keyboard
-    /// Keypad                   Keyboard
-    /// +-+-+-+-+                +-+-+-+-+
-    /// |1|2|3|C|                |1|2|3|4|
-    /// +-+-+-+-+                +-+-+-+-+
-    /// |4|5|6|D|                |Q|W|E|R|
-    /// +-+-+-+-+       =>       +-+-+-+-+
-    /// |7|8|9|E|                |A|S|D|F|
-    /// +-+-+-+-+                +-+-+-+-+
-    /// |A|0|B|F|                |Z|X|C|V|
-    /// +-+-+-+-+                +-+-+-+-+
-    pub fn map_key(k: &Mini_Key) -> Option<usize> {
-        match k {
-            Mini_Key::Key1 => Some(1),
-            Mini_Key::Key2 => Some(2),
-            Mini_Key::Key3 => Some(3),
-            Mini_Key::Key4 => Some(0xC),
-            Mini_Key::Q => Some(4),
-            Mini_Key::W => Some(5),
-            Mini_Key::E => Some(6),
-            Mini_Key::R => Some(0xD),
-            Mini_Key::A => Some(7),
-            Mini_Key::S => Some(8),
-            Mini_Key::D => Some(9),
-            Mini_Key::F => Some(0xE),
-            Mini_Key::Z => Some(0xA),
-            Mini_Key::X => Some(0),
-            Mini_Key::C => Some(0xB),
-            Mini_Key::V => Some(0xF),
-            _ => None,
-        }
-    }
-
     /// Handle the key down event for one of the 16 possible keys
     #[cfg(test)]
-    pub fn handle_key_down(&mut self, k: &Mini_Key) {
-        if let Some(idx) = Self::map_key(k) {
-            self.set_key_down(idx);
-        }
+    pub fn handle_key_down(&mut self, k: Key) {
+        self.set_key_down(k);
     }
 
     /// Handle the key down event for one of the 16 possible keys
-    fn set_key_down(&mut self, idx: usize) {
-        if idx <= 0xF {
-            self.key_input.insert(idx, true);
-        }
+    fn set_key_down(&mut self, k: Key) {
+        self.key_input.insert(k, true);
     }
 
     /// Handle the key up event for one of the 16 possible keys
     #[cfg(test)]
-    pub fn handle_key_up(&mut self, k: &Mini_Key) {
-        if let Some(idx) = Self::map_key(k) {
-            self.set_key_up(idx);
-        }
+    pub fn handle_key_up(&mut self, k: Key) {
+        self.set_key_up(k);
     }
 
     /// Handle the key down event for one of the 16 possible keys
-    fn set_key_up(&mut self, idx: usize) {
-        if idx <= 0xF {
-            self.key_input.insert(idx, false);
-        }
+    fn set_key_up(&mut self, k: Key) {
+        self.key_input.insert(k, false);
     }
 
     /// Given the indices of the keys pressed down on the system keyboard,
     /// fire the appropriate key_up and key_down handlers
-    pub fn update_keyboard_with_vec(&mut self, key_idxs: &[usize]) {
-        let set: HashSet<usize> = HashSet::from_iter(key_idxs.iter().cloned());
+    pub fn update_keyboard_with_vec(&mut self, keys: &[Key]) {
+        let set: HashSet<Key> = HashSet::from_iter(keys.iter().cloned());
 
         // check each of the 16 keys to see which have changed from up to down or vice versa
-        for i in 0..NUM_KEYS {
-            let system_key_is_down = set.contains(&i);
-            let interpreter_key_is_down = self.get_key_state(i);
+        for k in Key::variants() {
+            let system_key_is_down = set.contains(&k);
+            let interpreter_key_is_down = self.get_key_state(k);
 
             if system_key_is_down && !interpreter_key_is_down {
-                self.set_key_down(i);
+                self.set_key_down(k);
             } else if !system_key_is_down && interpreter_key_is_down {
-                self.set_key_up(i);
+                self.set_key_up(k);
             }
         }
     }
 
     /// Return the bool value of the bit at the given index
-    pub fn get_key_state(&self, idx: usize) -> bool {
-        self.key_input[&idx]
+    pub fn get_key_state(&self, k: Key) -> bool {
+        self.key_input[&k]
     }
 
     /// Called when the KeyOpGet Op is executed. The interpreter will transition out of
@@ -169,14 +146,15 @@ impl Keyboard {
     }
 
     /// Checks if we're in a blocking state, and if a valid key has been pressed
-    /// transitions out of the blocking state
-    pub fn handle_fx0a_state(&mut self, key_idxs: &[usize]) -> (Option<usize>, Option<u8>) {
+    /// transitions out of the blocking state. Returns a tuple with the register
+    /// to store the key in and the Key which was pressed
+    pub fn handle_fx0a_state(&mut self, keys: &[Key]) -> (Option<usize>, Option<u8>) {
         // check if the FX0A instruction has us in a blocking state and if we can now unblock
         if self.fx0a_metadata.should_block_on_keypress {
             // we choose of the first key because we have to choose SOME key, so why not the first?
-            key_idxs.get(0).map(|k| {
+            keys.get(0).map(|k: &Key| {
                 let reg_idx = self.unblock();
-                return (Some(reg_idx), Some(*k as u8));
+                return (Some(reg_idx), Some(*k));
             });
         }
         (None, None)
@@ -193,6 +171,5 @@ impl Keyboard {
 #[derive(Default, Debug, PartialEq)]
 struct FX0AMetadata {
     should_block_on_keypress: bool, // set to true if CPU is waiting on a keypress
-    last_key_pressed: Option<Mini_Key>,  // once a key is pressed store it here
     register: Option<u8>,           // the register to store the pressed key in
 }
